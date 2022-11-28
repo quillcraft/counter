16040aeda9a32c00500c1bf001dc26519219096f
Anton Mizinov <mizinov.anton@gmail.com>
2022-11-28 11:29:15 +0300

    Change function to class

---
+++ Counter.js
@@ -0,0 +1,46 @@
+export default class Counter {
+	constructor(props) {
+		this.root = props.root
+		this.number = props.number
+
+		this.format = /\d+/
+		this.doc = window.getComputedStyle(document.documentElement)
+		this.numWidth = +this.doc.getPropertyValue('--num-width').match(this.format)[0]
+		this.numHeight = +this.doc.getPropertyValue('--num-height').match(this.format)[0]
+		this.numDigits = +this.doc.getPropertyValue('--num-digits').match(this.format)[0]
+
+		this.counter = document.createElement('div')
+		this.counter.classList.add('counter')
+	}
+
+	init() {
+		String(this.number).split('').forEach((m, i) => {
+			const roller = document.createElement('div')
+			roller.classList.add('roller')
+			roller.style.top = `0px`
+			roller.style.left = `${i * this.numWidth}px`
+
+			const range = new Array(10).fill(0)
+			range.forEach((n, j) => {
+				const digit = document.createElement('div')
+				digit.classList.add('digit')
+
+				digit.textContent = j
+				roller.appendChild(digit)
+			})
+
+			this.counter.appendChild(roller)
+		})
+
+		this.root.appendChild(this.counter)
+	}
+
+	start() {
+		const rollers = Array.from(this.counter.children)
+		rollers.forEach((roller, i) => {
+			const m = String(this.number).split('')[i]
+			roller.style.top = `${-m * this.numHeight}px`
+			roller.style.left = `${i * this.numWidth}px`
+		})
+	}
+}
--- index.js
+++ index.js
@@ -1,53 +1,13 @@
+import Counter from './Counter.js'
+
 (function() {
     const app = document.querySelector('#app')
-    const range = new Array(10).fill(0)
- 
-    const format = /\d+/
-    const doc = window.getComputedStyle(document.documentElement)
-    const numWidth = +doc.getPropertyValue('--num-width').match(format)[0]
-    const numHeight = +doc.getPropertyValue('--num-height').match(format)[0]
-    const numDigits = +doc.getPropertyValue('--num-digits').match(format)[0]
 
-    let number = Math.round((10 ** numDigits) * Math.random())
-
-    const initCounter = () => {
-        const counter = document.createElement('div')
-        counter.classList.add('counter')
-
-        String(number).split('').forEach((m, i) => {
-            const roller = document.createElement('div')
-            roller.classList.add('roller')
-            roller.style.top = `${-m * numHeight}px`
-            roller.style.left = `${i * numWidth}px`
-
-            range.forEach((n, j) => {
-                const div = document.createElement('div')
-                div.classList.add('digit')
-                div.textContent = j
-                roller.appendChild(div)
-            })
-
-            counter.appendChild(roller)
-        })
-
-        app.appendChild(counter)
-    }
-
-    const updateCounter = () => {
-        const rollers = Array.from(document.querySelectorAll('.roller'))
-
-        number = Math.round((10 ** numDigits) * Math.random())
-        String(number).split('').forEach((m, i) => {
-            const roller = rollers[i]
-            roller.style.top = `${-m * numHeight}px`
-            roller.style.left = `${i * numWidth}px`
-        })
-    }
-
-    initCounter()
+	const counter = new Counter({ root: app, number: 876654 })
+	counter.init()
 
     const button = document.createElement('button')
-    button.textContent = 'refresh'
-    button.onclick = () => updateCounter()
+    button.textContent = 'START'
+    button.onclick = () => counter.start()
     app.appendChild(button)
 })()
